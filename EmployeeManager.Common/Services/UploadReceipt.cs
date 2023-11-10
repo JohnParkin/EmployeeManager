871575e361ca717baa39a0ed2c370b826b7aa0ba
John Parkin <johnparkin@oceanprojectmanagement.com>
2023-11-10 16:35:37 +0000

    Upload Restored :( no connection string.

--- EmployeeManager.Common/Services/UploadReceipt.cs
+++ EmployeeManager.Common/Services/UploadReceipt.cs
@@ -1,12 +1,16 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure.Core.Serialization;
+using Azure.Storage.Blobs;
 using EmployeeManager.Common.Model;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EmployeeManager.Common.Services
 {
@@ -17,14 +21,14 @@ namespace EmployeeManager.Common.Services
         string connection_string = "";
 
         string blobContainerName = "receipts-test1";
-
         private BlobContainerClient _blobContainerClient;
 
         public BlobUpload()
         {
             _blobContainerClient = new BlobContainerClient(connection_string, blobContainerName);
-            _filepath = "C:/Users/johnp/source/repos/Building Desktop Applications/EmployeeManager.WinUI/EmployeeManager.WinUI/Images/Scan 6.jpg";
+            _filepath = "C:/Users/johnp/source/repos/Building Desktop Applications/EmployeeManager.WinUI/EmployeeManager.WinUI/Images/Scan 1.jpg";
         }
+                         
         public async void UploadFileToBlobStorage()
         {
             Debug.WriteLine($"Reciept Upload");
@@ -34,6 +38,9 @@ namespace EmployeeManager.Common.Services
             BlobContainerClient containerClient,
             string localFilePath)
         {
+
+            _blobContainerClient = new BlobContainerClient(connection_string, blobContainerName);
+
             string fileName = Path.GetFileName(localFilePath);
             BlobClient blobClient = containerClient.GetBlobClient(fileName);
 
